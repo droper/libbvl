f31fc4164b1659079ba838b00b95d82d7db66f97
Pedro <pmunoz@gmail.com>
2013-04-02 03:27:30 -0500

    Agregado .gitignore

--- libbvl/config.py
+++ libbvl/config.py
@@ -3,7 +3,6 @@
 
 """Parámetros de la libreria"""
 
-RPJ_EMPRESAS = ['002597', 'CM0004', 'B20001', 'B20023', 'CM0006']
 DICT_EMPRESAS = {'002597':'Corona', 'CM0004':'Shougang', \
                  'B20001':'Castrovirreyna', 'B20023':'Perubar', \
                  'CM0006':'Cerro Verde'}
